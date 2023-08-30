4afbd73ebf4f40509dee1a418813840fe12342ec
jogiter ding <jogiter.g@gmail.com>
2023-08-30 19:15:10 +0800

    feat: add smart contract demo

---
+++ smart-contract/deploy.js
@@ -0,0 +1,39 @@
+import web3 from '../w3'
+import debug from 'debug'
+import Demo from './Demo.json'
+
+const log = debug('app:deploy')
+
+// Loading the contract ABI and Bytecode
+// (the results of a previous compilation step)
+
+async function main() {
+  const { abi, bytecode } = Demo;
+  // const { abi, bytecode } = JSON.parse(fs.readFileSync("sc/Demo.json"));
+  // Creating a signing account from a private key
+  const signer = web3.eth.accounts.privateKeyToAccount(
+    process.env.SIGNER_PRIVATE_KEY,
+  );
+  web3.eth.accounts.wallet.add(signer);
+
+  // Using the signing account to deploy the contract
+  const contract = new web3.eth.Contract(abi);
+  contract.options.data = bytecode;
+  const deployTx = contract.deploy();
+  const deployedContract = await deployTx
+    .send({
+      from: signer.address,
+      gas: await deployTx.estimateGas(),
+    })
+    .once("transactionHash", (txhash) => {
+      console.log(`Mining deployment transaction ...`);
+      console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+    });
+  // The contract is now deployed on chain!
+  console.log(`Contract deployed at ${deployedContract.options.address}`);
+  console.log(
+    `Add DEMO_CONTRACT to the.env file to store the contract address: ${deployedContract.options.address}`,
+  );
+}
+
+main();
